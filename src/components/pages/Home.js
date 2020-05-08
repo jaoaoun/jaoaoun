a398042ce77af31a16bac70ddff65c492f4d3acb
jaoaoun <ntd_pst@hotmail.com>
2020-05-08 18:09:23 +0700

    Create Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from 'react'
+import 'antd/dist/antd.css'
+import '../../css/Home.css'
+import { Carousel, Button } from 'antd'
 
 export const Home = () => {
   return (
     <div>
+      <div>
         <h1>Home</h1>
-      <p>Hi</p>
+      </div>
+      <div>
+        <Carousel dotPosition="right">
+          <div>
+            <p style={{ fontSize: '40px', textAlign: 'left', paddingLeft: '20px', height: '30px' }}>Hello</p>
+            <p style={{ fontSize: '35px', textAlign: 'left', paddingLeft: '20px', height: '30px' }}>Welcome to My Profile!!</p>
+            <Button 
+            style={{bottom:'-280px'}} 
+            shape="round"
+            size= "large"
+            >Resume(.pdf)</Button>
+          </div>
+          <div>
+            <p style={{ fontSize: '40px', textAlign: 'left', paddingLeft: '20px', height: '30px' }}>こんにちは</p>
+            <p style={{ fontSize: '35px', textAlign: 'left', paddingLeft: '20px', height: '30px' }}>私のプロフィールへようこそ!!</p>
+          </div>
+        </Carousel>
+      </div>
     </div>
   )
-};
+}
 
-export default Home;
+export default Home
