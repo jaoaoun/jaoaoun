d66b2bed83568762e212ba2d1a8d5ad6aa1de8e8
jaoaoun <ntd_pst@hotmail.com>
2020-05-09 22:17:59 +0700

    Create component and css

---
+++ src/components/pages/Education.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Education = () => {
+  return (
+    <div>
+        <h1>Education</h1>
+        <div >
+
+        </div>
+      
+    </div>
+  )
+};
+
+export default Education;
---
+++ src/components/pages/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Skills = () => {
+  return (
+    <div>
+        <h1>Skills</h1>
+        <div >
+
+        </div>
+      
+    </div>
+  )
+};
+
+export default Skills;
