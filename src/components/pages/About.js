aaab0674540ba31d3259c4b183d60a60f068b10c
jaoaoun <ntd_pst@hotmail.com>
2020-05-09 22:18:10 +0700

    Create About

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React from 'react'
+import 'antd/dist/antd.css'
+import '../../css/About.css'
+import { Card, Typography, Col, Row } from 'antd'
 
 export const About = () => {
+  const { Text } = Typography
   return (
-    <div>
-        <h1>About</h1>
-      <p>Hi</p>
+    <div className="about">
+      <h1>About</h1>
+      <div>
+        <Row>
+          <Col>
+            <img
+              src={require('../../img/about1.jpg')}
+              alt="logo"
+              align="center"
+              style={{ height: '447px', width: '500px', marginLeft: '25px', marginRight: '15px', marginTop: '30px' }}
+            />
+          </Col>
+          <Col>
+            <Card
+              title="About Me"
+              style={{ height: 'auto', width: '650px', marginTop: '30px' }}
+              headStyle={{ backgroundColor: '#CB9894' }}
+              bodyStyle={{ backgroundColor: '#EFCBC3' }}
+            >
+              <div>
+                <Text style={{ fontSize: '20px' }}>
+                  <p>Name: Nattida Pasutham</p>
+                  <p>Birthday: 19/01/1997</p>
+                  <p>Location: Bangkok,TH</p>
+                </Text>
+              </div>
+              <div>
+                <Text style={{ fontSize: '20px' }}>
+                  <Text style={{ marginLeft: '30px' }}>
+                    Hi! I'm graduate from information engineering of KMITL.
+                    <br />
+                  </Text>
+                  I'm interested in web development / fronend development.
+                  <br />
+                  I have attention and patience in working. I have ability to work <br />
+                  under pressure conditions. I have good human relationship and teamwork. Apart from coding and ability
+                  for work I also love to listening to music, eat, drawing, travel and take a photo.
+                </Text>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
-};
+}
 
-export default About;
+export default About
