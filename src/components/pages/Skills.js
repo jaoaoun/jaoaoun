fb1cfa737f807d6201fbb1d50a39f96d33188bf2
jaoaoun <ntd_pst@hotmail.com>
2020-05-10 22:32:29 +0700

    Add alt

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -21,29 +21,29 @@ export const Skills = () => {
       <div>
         <Card title="Computer" style={{margin:'30px'}} headStyle={{ backgroundColor: '#EFCBC3' }}>
           <Card.Grid style={gridStyle}>Microsoft office
-          <br/> <img src={require('../../img/micro-off.JPG')} style={{height:'auto', width:'250px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/micro-off.JPG')} alt="img-skills" style={{height:'auto', width:'250px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>Adobe photoshop
-          <br/> <img src={require('../../img/adobe-ps.JPG')} style={{height:'auto', width:'150px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/adobe-ps.JPG')} alt="img-skills" style={{height:'auto', width:'150px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>SVN
-          <br/> <img src={require('../../img/SVN.jpg')} style={{height:'auto', width:'150px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/SVN.jpg')} alt="img-skills" style={{height:'auto', width:'150px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>Matlab
-          <br/> <img src={require('../../img/Matlab.jpg')} style={{height:'auto', width:'150px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/Matlab.jpg')} alt="img-skills" style={{height:'auto', width:'150px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>HTML&CSS&JavaScript
-          <br/> <img src={require('../../img/hcj.jpg')} style={{height:'auto', width:'250px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/hcj.jpg')} alt="img-skills" style={{height:'auto', width:'250px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>Bootstrap
-          <br/> <img src={require('../../img/boot.jpg')} style={{height:'auto', width:'150px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/boot.jpg')} alt="img-skills" style={{height:'auto', width:'150px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>React&Reactstrap
-          <br/> <img src={require('../../img/React.png')} style={{height:'auto', width:'100px',marginTop:'20px'}}/>
-          <img src={require('../../img/reactstrap.JPG')} style={{height:'auto', width:'100px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/React.png')} alt="img-skills" style={{height:'auto', width:'100px',marginTop:'20px'}}/>
+          <img src={require('../../img/reactstrap.JPG')} alt="img-skills" style={{height:'auto', width:'100px',marginTop:'20px'}}/>
           </Card.Grid>
           <Card.Grid style={gridStyle}>Ant design
-          <br/> <img src={require('../../img/antd.png')} style={{height:'auto', width:'150px',marginTop:'20px'}}/>
+          <br/> <img src={require('../../img/antd.png')} alt="img-skills" style={{height:'auto', width:'150px',marginTop:'20px'}}/>
           </Card.Grid>
         </Card>
       </div>
@@ -53,7 +53,7 @@ export const Skills = () => {
           <Card.Grid style={gridStyle2}>Thai(Native)</Card.Grid>
           <Card.Grid style={gridStyle2}>Japanese(Basic)</Card.Grid>
           <Card.Grid style={gridStyle}>Korean(Basic)
-          <br/> <img src={require('../../img/Cer-ko.JPG')} style={{height:'auto', width:'200px',marginTop:'20px'}} /> 
+          <br/> <img src={require('../../img/Cer-ko.JPG')} alt="img-skills" style={{height:'auto', width:'200px',marginTop:'20px'}} /> 
           </Card.Grid>
         </Card>
         </div>
