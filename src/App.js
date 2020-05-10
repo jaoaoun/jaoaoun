07147dea5205d6ec1740b4d66d6d64e82a93eb4f
jaoaoun <ntd_pst@hotmail.com>
2020-05-10 21:20:15 +0700

    Add component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ProfileBar from './components/layout/ProfileBar';
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Skills from './components/pages/Skills'
+import Education from './components/pages/Education'
+import Experience from './components/pages/Experience'
+import Contact from './components/pages/Contact'
 
 
 
@@ -21,6 +25,18 @@ function App() {
             </Route>
             <Route path="/About">
               <About/>
+            </Route>
+            <Route path="/Skills">
+              <Skills/>
+            </Route>
+            <Route path="/Education">
+              <Education/>
+            </Route>
+            <Route path="/Experience">
+              <Experience/>
+            </Route>
+            <Route path="/Contact">
+              <Contact/>
             </Route>
           </Switch>
         </div>
