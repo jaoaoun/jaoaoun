0cf91c9b6f2d9688287226c3ebddb3cb4f2a884a
jaoaoun <ntd_pst@hotmail.com>
2020-05-14 23:18:19 +0700

    Revise click menu to id

--- src/App.js
+++ src/App.js
@@ -1,50 +1,42 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import ProfileBar from './components/layout/ProfileBar';
-import Home from './components/pages/Home'
-import About from './components/pages/About'
-import Skills from './components/pages/Skills'
-import Education from './components/pages/Education'
-import Experience from './components/pages/Experience'
-import Contact from './components/pages/Contact'
-
-
+import ProfileBar from './components/layout/ProfileBar'
+// import Home from './components/pages/Home'
+// import About from './components/pages/About'
+// import Skills from './components/pages/Skills'
+// import Education from './components/pages/Education'
+// import Experience from './components/pages/Experience'
+// import Contact from './components/pages/Contact'
 
 function App() {
   return (
-    
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/">
-              <ProfileBar/>
-            </Route>
-            <Route path="/Home">
-              <Home/>
-            </Route>
-            <Route path="/About">
-              <About/>
-            </Route>
-            <Route path="/Skills">
-              <Skills/>
-            </Route>
-            <Route path="/Education">
-              <Education/>
-            </Route>
-            <Route path="/Experience">
-              <Experience/>
-            </Route>
-            <Route path="/Contact">
-              <Contact/>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-
-      
-    
-  );
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <ProfileBar />
+          </Route>
+{/*           
+          <Route path="/About">
+            <About />
+          </Route>
+          <Route path="/Skills">
+            <Skills />
+          </Route>
+          <Route path="/Education">
+            <Education />
+          </Route>
+          <Route path="/Experience">
+            <Experience />
+          </Route>
+          <Route path="/Contact">
+            <Contact />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/components/layout/ProfileBar.js
+++ src/components/layout/ProfileBar.js
@@ -3,14 +3,13 @@ import 'antd/dist/antd.css'
 import '../../css/ProfileBar.css'
 import { Layout, Avatar, Menu, Col, Row } from 'antd'
 import MenuItem from 'antd/lib/menu/MenuItem'
-// import { useHistory } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Skills from '../pages/Skills'
 import Education from '../pages/Education'
 import Experience from '../pages/Experience'
 import Contact from '../pages/Contact'
-import { Link, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Slide from 'react-reveal'
 
 export const ProfileBar = () => {
@@ -18,9 +17,8 @@ export const ProfileBar = () => {
     padding: '10px',
     backgroundColor: 'rgb(204,194,193)'
   }
-  
-  const { Content, Footer, Sider } = Layout
 
+  const { Content, Footer, Sider } = Layout
 
   return (
     <div>
@@ -36,52 +34,44 @@ export const ProfileBar = () => {
           }}
         >
           <div style={imgProfileStyle}>
-            <Avatar size={180} img src={require('../../img/IMG_2182.jpgcrop.jpg')} shape="circle" />
+            <Avatar size={180} img src={require('../../assets/img/IMG_2182.jpgcrop.jpg')} shape="circle" />
             <p style={{ fontSize: '20px', textAlign: 'center', margin: '0' }}>Nattida Pasutham</p>
             <p style={{ fontSize: '15px', textAlign: 'center', margin: '0' }}>Software Engineer</p>
           </div>
-
-          <Menu backgroundColor="rgb(236,231,225)" mode="inline" defaultSelectedKeys={['1']}>
-            <MenuItem key="1">
-              <Link to="/Home">Home</Link>
-            </MenuItem>
-            <MenuItem key="2">
-              <Link to="About" section="About" />
-              About
-            </MenuItem>
-            <MenuItem key="3">
-              <Link to="/Skills" />
-              Skills
-            </MenuItem>
-            <MenuItem key="4">
-              <Link to="/Education" />
-              Education
-            </MenuItem>
-            <MenuItem key="5">
-              <Link to="/Experience" />
-              Experience
-            </MenuItem>
-            <MenuItem key="6">
-              <Link to="/Contact" />
-              Contact
-            </MenuItem>
-          </Menu>
+          <div className="nav">
+            <Menu backgroundColor="rgb(236,231,225)" mode="inline" defaultSelectedKeys={['1']}>
+              <MenuItem key="1">
+                <a href="#home">Home</a>
+              </MenuItem>
+              <MenuItem key="2">
+                <a href="#about"> About</a>
+              </MenuItem>
+              <MenuItem key="3">
+                <a href="#skills">Skills </a>
+              </MenuItem>
+              <MenuItem key="4">
+                <a href="#education">Education</a>
+              </MenuItem>
+              <MenuItem key="5">
+                <a href="#experience">Experience</a>
+              </MenuItem>
+              <MenuItem key="6">
+                <a href="#contact">Contact</a>
+              </MenuItem>
+            </Menu>
+          </div>
         </Sider>
         <Layout>
-        
           <div style={{ margin: 24 }}>
             <Content style={{ padding: 0 }}>
               <div className="site-layout-background" style={{ padding: 24, minHeight: 720 }}>
-                <React.Fragment>
+                <Route>
                   <Slide bottom className="reveal-page1">
-                    <section id="Home">
-                      <Home />
-                    </section>
+                    <Home />
                   </Slide>
+
                   <Slide bottom className="reveal-page2">
-                    <section id="About">
-                      <About />
-                    </section>
+                    <About />
                   </Slide>
                   <Slide bottom className="reveal-page2">
                     <Skills />
@@ -95,16 +85,7 @@ export const ProfileBar = () => {
                   <Slide bottom className="reveal-page2">
                     <Contact />
                   </Slide>
-                </React.Fragment>
-                {/* <Route path="/" component={Home}/> */}
-
-                
-                <Route path="/Home" component={Home} />
-                <Route path="/About" component={About} />
-                <Route path="/Skills" component={Skills}/>
-                <Route path="/Education" component={Education} />
-                <Route path="/Experience" component={Experience} />
-                <Route path="/Contact" component={Contact} />
+                </Route>
               </div>
             </Content>
           </div>
