b8ba551074b7c197117f6725db071bafd7716702
jaoaoun <ntd_pst@hotmail.com>
2020-05-18 01:37:37 +0700

    Updates

---
+++ precache-manifest.2aa731394cd90fbaae3c70f2d51b7f5a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91726ce1d5ec9a9951adae3608eee896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9cf074c9d7b6a97ae36",
+    "url": "/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "f2cd71e6affb75b02805",
+    "url": "/static/css/main.10177824.chunk.css"
+  },
+  {
+    "revision": "b9cf074c9d7b6a97ae36",
+    "url": "/static/js/2.29ee2313.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.29ee2313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2cd71e6affb75b02805",
+    "url": "/static/js/main.713674a5.chunk.js"
+  },
+  {
+    "revision": "3a9afd63113e20012ee4",
+    "url": "/static/js/runtime-main.0a75d356.js"
+  },
+  {
+    "revision": "834948f2064ac13af17627210a681430",
+    "url": "/static/media/CAN.834948f2.JPG"
+  },
+  {
+    "revision": "210e9b7c470ed3e5e78352f78d0048d9",
+    "url": "/static/media/Cer-ko.210e9b7c.JPG"
+  },
+  {
+    "revision": "4e6e2d3a47cd7a7c8751c6cf5c851877",
+    "url": "/static/media/GCPR3484.4e6e2d3a.JPEG"
+  },
+  {
+    "revision": "efbe7f084f6ee0620003c731eafcf69f",
+    "url": "/static/media/IMG_2182.jpgcrop.efbe7f08.jpg"
+  },
+  {
+    "revision": "3e61f23432f797df88ffd01eb5a9e534",
+    "url": "/static/media/Matlab.3e61f234.jpg"
+  },
+  {
+    "revision": "67b63e0b67683b5a066a1759dba221df",
+    "url": "/static/media/Nattida_Pst.67b63e0b.pdf"
+  },
+  {
+    "revision": "3d92e90617cc8398c00939bed7e2412e",
+    "url": "/static/media/SVN.3d92e906.jpg"
+  },
+  {
+    "revision": "fe5925e4132efa3bb565dd9cc09e3d51",
+    "url": "/static/media/about1.fe5925e4.jpg"
+  },
+  {
+    "revision": "b51e4192308e19afe2aeee4ad65ec18a",
+    "url": "/static/media/adobe-ps.b51e4192.JPG"
+  },
+  {
+    "revision": "a5a8009154afa167850a165bea0bef99",
+    "url": "/static/media/au2_n.a5a80091.jpg"
+  },
+  {
+    "revision": "2a46e5da2ea392d1ac999b0a74dc947f",
+    "url": "/static/media/boot.2a46e5da.jpg"
+  },
+  {
+    "revision": "caea0d85f9e7f02c2e25dc60ab056551",
+    "url": "/static/media/ch_n.caea0d85.jpg"
+  },
+  {
+    "revision": "c590418ea2b1843edc4c70b9df583d2a",
+    "url": "/static/media/cru.c590418e.png"
+  },
+  {
+    "revision": "c5e34979fde4f26c5554da039212d677",
+    "url": "/static/media/education.c5e34979.png"
+  },
+  {
+    "revision": "4cd8e3c8ee61bbc14689db9f5c34b356",
+    "url": "/static/media/github.4cd8e3c8.png"
+  },
+  {
+    "revision": "f4556ff698c977f6655bc2ef9ba84d14",
+    "url": "/static/media/hcj.f4556ff6.jpg"
+  },
+  {
+    "revision": "19a906b65a54a469ba01dcb1daf09f62",
+    "url": "/static/media/home2.19a906b6.jpg"
+  },
+  {
+    "revision": "45d52550e24f6da34749d64ca1ee9a40",
+    "url": "/static/media/ice.45d52550.jpg"
+  },
+  {
+    "revision": "b26e0a59dc0b4ebcc867933466b98586",
+    "url": "/static/media/ite-logo.b26e0a59.png"
+  },
+  {
+    "revision": "2f63ebee3ed7b1c3a3e27f993268f9f7",
+    "url": "/static/media/kmi.2f63ebee.jpg"
+  },
+  {
+    "revision": "0e95d76e0dfdc3958acd033f022f6a02",
+    "url": "/static/media/mail.0e95d76e.jpg"
+  },
+  {
+    "revision": "343af72a862ed13230041c424c80dd3e",
+    "url": "/static/media/micro-off.343af72a.JPG"
+  },
+  {
+    "revision": "93ac91b5108d04e431e7734ecac57b62",
+    "url": "/static/media/project.93ac91b5.jpg"
+  },
+  {
+    "revision": "ebe2c809c1bea69926d030661d4685ad",
+    "url": "/static/media/reactstrap.ebe2c809.JPG"
+  },
+  {
+    "revision": "5b34f51fcfd272b5de811cf8e0f035ff",
+    "url": "/static/media/sal2_n.5b34f51f.jpg"
+  },
+  {
+    "revision": "937031f418f632714088265e0f16deaf",
+    "url": "/static/media/webresume.937031f4.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.com/jaoaoun/precache-manifest.ac3f94396e7f59b086aa04f6f8fb7064.js"
+  "/precache-manifest.2aa731394cd90fbaae3c70f2d51b7f5a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/jaoaoun/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
